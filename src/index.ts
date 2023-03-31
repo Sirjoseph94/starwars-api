99973c0c60926c1d2b08cf7d0947594fb54c3200
Joseph Ero <erojoseph94@gmail.com>
2023-04-01 00:23:20 +0100

    added middlewares to handle cors, http security headers, and allow express process json request payload

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,24 @@
 import express from "express";
 import dotenv from "dotenv";
+import helmet from "helmet";
+import cors from "cors"
+
 dotenv.config();
 
 const PORT = process.env.PORT || 7001;
 const app = express();
+
+app.use(cors())
+app.use(helmet())
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
+
+app.use((_req, res) =>
+  res
+    .status(404)
+    .json({ status: "Not Found", message: "This route does not exist" })
+);
 
 app.listen(PORT, () => {
   console.log(`Server live at PORT: ${PORT}`);

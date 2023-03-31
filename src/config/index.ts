a7592786430a7f064de98bf29c26b86ca0c187f1
Joseph Ero <erojoseph94@gmail.com>
2023-04-01 00:42:54 +0100

    created a config file for all constants and config variables

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,10 +1,8 @@
-
 import dotenv from "dotenv";
 dotenv.config;
 
 const CONFIG = {
   PORT: Number(process.env.PORT) || 7000,
-  API_BASE_URL: "https://swapi.dev/api/",
+  API_BASE_URL: process.env.API_URL,
 };
-
-export default CONFIG
+export default CONFIG;

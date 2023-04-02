ef7ae525bca49a51ca5e7eedb5d0a36529279edb
Joseph Ero <erojoseph94@gmail.com>
2023-04-02 23:48:30 +0100

    Update index.ts

--- src/config/index.ts
+++ src/config/index.ts
@@ -3,6 +3,6 @@ dotenv.config();
 
 const CONFIG = {
   PORT: Number(process.env.PORT) || 7000,
-  API_BASE_URL: process.env.API_URL,
+  API_BASE_URL: "https://swapi.dev/api/",
 };
 export default CONFIG;

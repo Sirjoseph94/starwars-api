8b576c823d2b5463ba9e78f37cb187793c1cee5c
Joseph Ero <erojoseph94@gmail.com>
2023-04-02 05:36:14 +0100

    Wrote a basic README file

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
-# starwars-api
-Starwars movies and characters API using Express.js and Typescript
+
+#  STARWARS API
+This is a simple REST API built with Express.js that consumes the Star Wars API (SWAPI) to retrieve information about the Star Wars universe. The API endpoints allow you to retrieve information about characters, films, and make your comments.
+
+  
+##  Prerequisites
+
+To run this project, you'll need to have Node.js and npm installed on your machine. You'll also need to rename `example.env` to `.env` file in the root directory of the project with the following environment variables:
+
+    PORT = 3000
+    NODE_ENV = development
+    API_URL =  "https://swapi.dev/api/"
+    DATABASE_URL= your postgres database url string
+    
+## Getting Started
+
+To get started, clone this repository to your local machine and run the following command to install the dependencies:
+
+    npm install
+ Once the dependencies are installed, run the following command to start the server:
+ 
+
+    npm run prepare
+    npm run start
+
+## API Endpoints
+
+The API documentation of the availible endpoints can be found here
+[API DOC](http://localhost:3000/api-doc)
+
+## Live DEMO
+The live demo is hosted here
+[Live demo link](https://link%20to%20live%20demo)
--- src/services/comments.ts
+++ src/services/comments.ts
@@ -1,37 +1,45 @@
-import db from "../config/dbConfig.js"
+import db from "../config/dbConfig.js";
+import ip from "ip";
 
-type newComment = {
-    ipAddress: string;
-    filmId: number;
-    text: string;
-}
-
-export const newComment = async(payload: newComment)=>{
+export const newComment = async (payload: Record<string, any>) => {
   try {
-    const response =  await db.comment.create({
+    const response = await db.comment.create({
       data: {
-        ipAddress: payload.ipAddress,
+        ipAddress: ip.address(),
         text: payload.text,
-        filmId: payload.filmId
-      }
-    })
-    return {statusCode: 201, message: response}
+        filmId: payload.filmId,
+      },
+    });
+    return { statusCode: 201, message: response };
   } catch (error) {
-    console.error(error)
+    console.error(error);
     throw { statusCode: 500, message: error };
   }
-}
+};
 
-export const getComments =async (filmId:number) => {
+export const getComments = async (filmId: number) => {
   try {
     const comments = await db.comment.findMany({
-      where:{
-        filmId
-      }
-    })
-    return {statusCode: 200, message: comments}
+      where: {
+        filmId,
+      },
+    });
+    return { statusCode: 200, message: comments };
   } catch (error) {
-     console.error(error);
-     throw { statusCode: 500, message: error };
+    console.error(error);
+    throw { statusCode: 500, message: error };
   }
-}
+};
+export const listComments = async () => {
+  try {
+    const comments = await db.comment.findMany({
+      orderBy: {
+        createdAt: "desc",
+      },
+    });
+    return { statusCode: 200, message: comments };
+  } catch (error) {
+    console.error(error);
+    throw { statusCode: 500, message: error };
+  }
+};
